4fb66328df3592738f9149cf63697c5fc97f79d5
Illessar Sapphiron <illyavrora@gmail.com>
2016-01-03 05:47:38 +0100

    lol

---
+++ src/test/java/Calcul/Calcul/CalculMetierTest.java
@@ -0,0 +1,24 @@
+package Calcul.Calcul;
+
+import junit.framework.TestCase;
+
+public class CalculMetierTest extends TestCase {
+	
+	CalculMetier metier;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		metier = new CalculMetier();
+	}
+	
+	public void testSomme()
+	{
+		assertTrue(metier.somme(10, 9) == 19);
+	}
+	
+	public void testProduit()
+	{
+		assertTrue(metier.produit(10, 9) == 90);
+	}
+
+}
